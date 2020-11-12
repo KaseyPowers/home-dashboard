28b431db4c2877f90c989a76f1f6669c18705086
OObadare <ogundipe.obadare@gmail.com>
2020-11-12 17:56:39 -0500

    plug into react /hackerman

--- src/widgets/ClubhouseWidget/ClubhouseWidget.js
+++ src/widgets/ClubhouseWidget/ClubhouseWidget.js
@@ -1,45 +1,60 @@
+import {useEffect, useState} from "react";
 // Sign up for clubhouse api token https://github.com/clubhouse/api-cookbook/blob/main/set-up-instructions.md#setting-your-environment-variable
 
 import WidgetCard from "../WidgetCard";
 import { CardContent, Typography, Link } from "@material-ui/core";
 
+const getUser = async () => {
+  const response = await fetch("https://api.clubhouse.io/api/v3/member", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Clubhouse-Token": process.env.REACT_APP_CLUBHOUSE_API_TOKEN,
+    }
+  });
+  return response.json()
+}
+
+const getStories = async () => {
+  let userData = await getUser();
+  let name = userData.mention_name;
+  const response = await fetch(`https://api.clubhouse.io/api/v3/search/stories?query=owner:${name}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Clubhouse-Token": process.env.REACT_APP_CLUBHOUSE_API_TOKEN,
+    },
+  });
+  return response.json();
+}
 
 const ClubhouseWidget = (props) => {
+  const [stories, setStories] = useState({});
 
-  const getUser = async () => {
-    const response = await fetch("https://api.clubhouse.io/api/v3/member", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Clubhouse-Token": process.env.REACT_APP_CLUBHOUSE_API_TOKEN,
+  useEffect(() => {
+    let canceled = false;
+
+    const fn = async () => {
+      const storiesJson = await getStories();
+      const newStories = storiesJson.data.reduce((output, story) => {
+        output[story.id] = {
+          url: story.app_url,
+          name: story.name
+        };
+        return output;
+      }, {});
+
+      if (!canceled) {
+        setStories(newStories);
       }
-    });
-    return response.json()
-  } 
+    }
 
-  const getStories = async() => {
-    let userData = await getUser();
-    let name = userData.mention_name;
-    const response = await fetch(`https://api.clubhouse.io/api/v3/search/stories?query=owner:${name}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Clubhouse-Token": process.env.REACT_APP_CLUBHOUSE_API_TOKEN,
-      },
-    });
-    return response.json();
-  }
+    fn();
 
-  const stories = getStories().then( json => {
-    let storyObj = {}
-    json.data.forEach(story => {
-      stories[story.id] = {
-        url: story.app_url,
-        name: story.name,
-      }
-    })
-    return storyObj;
-  });
+    return () => {
+      canceled = true;
+    };
+  }, []);
   
 
   return(
